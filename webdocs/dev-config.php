90735177774a82ca36746ceb3fb63fd62efd93ab
Jason Crane <jasonlcrane@gmail.com>
2013-08-04 23:16:18 -0500

    rm

--- webdocs/dev-config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	define( 'DB_NAME',     'pecanlodge_dev_wp');
-	define( 'DB_USER',     'pecanlodge');
-	define( 'DB_PASSWORD', '0H2C3LCPEw!Z[7q');
-	define( 'DB_HOST',     'localhost'  );
-?>
